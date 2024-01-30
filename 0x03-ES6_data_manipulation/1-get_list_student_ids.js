9708f12ecd56643a4aa0450b71301132f943493c
Chidiebere Chukwuma <chukschiboy@gmail.com>
2024-01-30 01:00:06 +0000

    function getListStudentIds that returns an array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,11 @@
-export default function getListStudentIds(array) { 
-   if (!Array.isArray(array)) return []; 
-   return array.map((i) => i.id);
+// 1-get_list_student_ids.js
+
+export default function getListStudentIds(arrayOfObjects) {
+  // Check if the argument is an array
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+
+  // Use the map function to extract ids from the array of objects
+  return arrayOfObjects.map(student => student.id);
+}
